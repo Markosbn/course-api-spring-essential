d20d55857c23e1487ec7fc0647b2bf6cfa461ef6
Marcos Della Justina Schulz <markosdjs@gmail.com>
2022-04-04 23:34:19 -0300

    Implementado valid campos e handler

--- src/main/java/com/schulz/apispringessential/requests/AnimePostRequestBody.java
+++ src/main/java/com/schulz/apispringessential/requests/AnimePostRequestBody.java
@@ -2,8 +2,11 @@ package com.schulz.apispringessential.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class AnimePostRequestBody {
 
+    @NotEmpty(message = "The anime name cannot be empty")
     private String name;
 }
--- src/main/java/com/schulz/apispringessential/requests/AnimePutRequestBody.java
+++ src/main/java/com/schulz/apispringessential/requests/AnimePutRequestBody.java
@@ -2,8 +2,12 @@ package com.schulz.apispringessential.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class AnimePutRequestBody {
     private Long id;
+
+    @NotEmpty(message = "The anime name cannot be empty")
     private String name;
 }
