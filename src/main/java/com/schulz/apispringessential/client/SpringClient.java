ee294e040820bf9249d3e7736d45236b7c159ade
Marcos Della Justina Schulz <markosdjs@gmail.com>
2022-04-05 23:09:22 -0300

    wevmvccontroller

---
+++ src/main/java/com/schulz/apispringessential/client/SpringClient.java
@@ -0,0 +1,4 @@
+package com.schulz.apispringessential.client;
+
+public class SpringClient {
+}
---
+++ src/main/java/com/schulz/apispringessential/configurer/SchulzWebMvcConfigurer.java
@@ -0,0 +1,21 @@
+package com.schulz.apispringessential.configurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class SchulzWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver pageHandler = new PageableHandlerMethodArgumentResolver();
+        pageHandler.setFallbackPageable(PageRequest.of(1, 5, Sort.Direction.ASC, "Name"));
+        resolvers.add(pageHandler);
+    }
+}
